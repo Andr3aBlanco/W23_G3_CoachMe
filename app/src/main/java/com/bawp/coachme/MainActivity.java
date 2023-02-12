a0f89bd78b82052aac03456e8a6573225ae344ca
Luis Miguel Miranda <lmirandad27@gmail.com>
2023-02-12 12:48:43 -0800

    Commit 2023-02-12 (by Luis M.): - Adding SDK Firebase - Adding Connectors to CoachMe Database

--- app/src/main/java/com/bawp/coachme/MainActivity.java
+++ app/src/main/java/com/bawp/coachme/MainActivity.java
@@ -1,7 +1,6 @@
 package com.bawp.coachme;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
