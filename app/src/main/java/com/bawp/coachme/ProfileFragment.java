d87ae946e622e770640ab82a4bd95b881e878f1b
jaydipkmulani <jaydipkmulani@gmail.com>
2023-03-16 11:12:27 -0700

    all activity paths is redirected to main activity needs to create signout btn  to sign out from firebase and to the main  page

--- app/src/main/java/com/bawp/coachme/ProfileFragment.java
+++ app/src/main/java/com/bawp/coachme/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -49,11 +50,15 @@ public class ProfileFragment extends Fragment {
 //            mParam2 = getArguments().getString(ARG_PARAM2);
 //        }
 //    }
-
+Button signOutBtn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+//        signOutBtn= container.findViewById(R.id.);
+
         return inflater.inflate(R.layout.fragment_profile, container, false);
+
+
     }
 }
