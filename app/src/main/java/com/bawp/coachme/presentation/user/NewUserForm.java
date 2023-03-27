e53ab4ed64a331e8e3babcade90ee97a3e59c780
jaydipkmulani <jaydipkmulani@gmail.com>
2023-03-27 10:58:54 -0700

    current location setted up
    added current location for the new user form

--- app/src/main/java/com/bawp/coachme/presentation/user/NewUserForm.java
+++ app/src/main/java/com/bawp/coachme/presentation/user/NewUserForm.java
@@ -1,10 +1,25 @@
 package com.bawp.coachme.presentation.user;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
@@ -19,7 +34,18 @@ import com.bawp.coachme.R;
 import com.bawp.coachme.model.Role;
 import com.bawp.coachme.model.User;
 import com.bawp.coachme.utils.UserSingleton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -29,28 +55,35 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import adapter.PlaceAutoSuggestionAdapter;
 
 public class NewUserForm extends AppCompatActivity {
     EditText firstNameTxt;
     EditText lastNameTxt;
-    AutoCompleteTextView addressTxt;
+    EditText addressTxt;
     EditText phoneNumberTxt;
-
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private final static int REQUEST_CODE = 100;
     String emailTxt;
 
     Button confirmDataBtn;
     FirebaseAuth auth;
-
+Button btnCurrent_location;
     FirebaseUser user;
     String current_User;
     String fname;
     String lname;
     String phonenum;
-
+private String city;
     Button btn;
     DatabaseReference databaseRef;
-
+    private LocationRequest locationRequest;
+    private Geocoder geocoder;
+    List<Address> addresses=null;
     public void onStart() {
         super.onStart();
         databaseRef = FirebaseDatabase.getInstance().getReference().child("users").child(current_User);
@@ -75,7 +108,7 @@ public class NewUserForm extends AppCompatActivity {
             }
         });
     }
-
+//creating user in database and setting up new
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -91,14 +124,15 @@ public class NewUserForm extends AppCompatActivity {
         confirmDataBtn = findViewById(R.id.btnConfirm);
         firstNameTxt = findViewById(R.id.txtFirstName);
         lastNameTxt = findViewById(R.id.txtLastName);
+        btnCurrent_location=findViewById(R.id.txtGetCurrentLocation);
 
         addressTxt = findViewById(R.id.txtAddress);
         phoneNumberTxt = findViewById(R.id.txtPhoneNumber);
-//        emailTxt=findViewById(R.id.txt_email_new_UF);
 
-
-        addressTxt.setAdapter(new PlaceAutoSuggestionAdapter(NewUserForm.this, android.R.layout.simple_list_item_1));
-
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(2000);
         // if user is not logged in we will open login activity
         if (user == null) {
             Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
@@ -113,8 +147,10 @@ public class NewUserForm extends AppCompatActivity {
             phoneNumberTxt.setText(phonenum);
 
         }
-        // whem exsiting user gets log in it will redirect them to main activity
-
+       //getting current location
+btnCurrent_location.setOnClickListener((View v)-> {
+getCurrentLocation();
+});
         confirmDataBtn.setOnClickListener((View v) -> {
 
 
@@ -166,4 +202,138 @@ public class NewUserForm extends AppCompatActivity {
 
 
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                if (isGPSEnabled()) {
+
+                    getCurrentLocation();
+
+                }else {
+
+                    turnOnGPS();
+                }
+            }
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 2) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                getCurrentLocation();
+            }
+        }
+    }
+
+    private void getCurrentLocation() {
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(NewUserForm.this, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                if (isGPSEnabled()) {
+
+                    LocationServices.getFusedLocationProviderClient(NewUserForm.this)
+                            .requestLocationUpdates(locationRequest, new LocationCallback() {
+                                @Override
+                                public void onLocationResult(@NonNull LocationResult locationResult) {
+                                    super.onLocationResult(locationResult);
+
+                                    LocationServices.getFusedLocationProviderClient(NewUserForm.this)
+                                            .removeLocationUpdates(this);
+
+                                    if (locationResult != null && locationResult.getLocations().size() >0){
+
+                                        int index = locationResult.getLocations().size() - 1;
+                                        double latitude = locationResult.getLocations().get(index).getLatitude();
+                                        double longitude = locationResult.getLocations().get(index).getLongitude();
+
+                                        geocoder = new Geocoder(NewUserForm.this, Locale.getDefault());
+
+                                        try {
+                                            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+                                        } catch (IOException e) {
+                                            throw new RuntimeException(e);
+                                        }
+                                        addressTxt.setText(addresses.get(0).getAddressLine(0));
+                                        Toast.makeText(NewUserForm.this, addresses.get(0).getAddressLine(0), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }, Looper.getMainLooper());
+
+                } else {
+                    turnOnGPS();
+                }
+
+            } else {
+                requestPermissions(new String[]{ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+    }
+
+    private void turnOnGPS() {
+
+
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(getApplicationContext())
+                .checkLocationSettings(builder.build());
+
+        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    Toast.makeText(NewUserForm.this, "GPS is already tured on", Toast.LENGTH_SHORT).show();
+
+                } catch (ApiException e) {
+
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+
+                            try {
+                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                resolvableApiException.startResolutionForResult(NewUserForm.this, 2);
+                            } catch (IntentSender.SendIntentException ex) {
+                                ex.printStackTrace();
+                            }
+                            break;
+
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            //Device does not have location
+                            break;
+                    }
+                }
+            }
+        });
+
+    }
+
+    private boolean isGPSEnabled() {
+        LocationManager locationManager = null;
+        boolean isEnabled = false;
+
+        if (locationManager == null) {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        }
+
+        isEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        return isEnabled;
+
+    }
+
 }
